dedfeda718738e72dad33a8340b769c67e544776
abhiasap <abhishekanil033@gmail.com>
2024-12-11 11:31:24 +0530

    intial commit

---
+++ add.js
@@ -0,0 +1,27 @@
+let a=5;
+let b=10;
+
+function add(a,b){
+
+    let c=a+b;
+    console.log(c);
+    
+}
+add(a,b);
+
+let name ="Abhishek Anil";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charCodeAt(4));
+let text = "HELLO WORLD";
+let char = text.charCodeAt(0);
+console.log(char);
+
+let letter = name.at(2);
+console.log(letter);
+let text2 = "Apple, Banana, Kiwi";
+let part = name.slice(2, 12);
+console.log(part);
+let text1 = "Hello World!";       // String
+let text4= text1.toLowerCase();  // text2 is text1 converted to lower
+console.log(text4);
